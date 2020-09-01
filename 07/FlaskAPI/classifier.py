61393310c8924b0c41be42030ac483a27a339745
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-01 19:03:16 +0900

    Define a class named 'Classfier'.

--- 07/FlaskAPI/classifier.py
+++ 07/FlaskAPI/classifier.py
@@ -13,3 +13,26 @@ def create_network():
     fc_input_dim = net.fc.in_features
     net.fc = nn.Linear(fc_input_dim, 2)
     return net
+
+
+# Define a class named 'Classifier'.
+class Classifier(object):
+    def __init__(self, params):
+        # Create Discrimination-Network.
+        self.net = create_network()
+
+        # Set the parameter of a procedure of training-accomplished
+        self.net.load_state_dict(params)
+
+        # Set as Evaluation-Mode.
+        self.net.eval()
+
+        # A function that converts Image to the Tensor.
+        self.transformer = transforms.Compose([
+            transforms.CenterCrop(224),
+            transforms.ToTensor()
+        ])
+
+        # Connect Classification-ID and Name.
+        self.classes = ["burrito", "taco"]
+
