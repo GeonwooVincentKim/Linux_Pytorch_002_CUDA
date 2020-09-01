dbe5da3e2858e00f92b8522747535448dd4fb077
kdsnop@gmail.com <kdsnop@gmail.com>
2020-09-01 19:18:16 +0900

    Generate an application contains a Classifier-class and a Flask-class.

--- 07/FlaskAPI/wsgi.py
+++ 07/FlaskAPI/wsgi.py
@@ -3,6 +3,18 @@ import torch
 from smart_getenv import getenv
 
 from .app import create_app
-from .app.classifier import Classifier
+from .classifier import Classifier
 
 
+# Read Parameter-File of routes
+# in Environment-Variable.
+prm_file = getenv("PRM_FILE", default="/taco_burrito.prm")
+
+# Read Parameter-Files.
+params = torch.load(prm_file,
+                    map_location=lambda storage, loc: storage)
+
+# Generate an application
+# that contains Classifier and Flask.
+classifier = Classifier(params)
+app = create_app(classifier)
